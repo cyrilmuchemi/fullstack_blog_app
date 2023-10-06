614737e11175b34810338d6d30a81f735934ec2c
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-10-06 09:32:24 +0300

    Add toggle theme slider to header

--- app/pages/includes/header.php
+++ app/pages/includes/header.php
@@ -11,28 +11,39 @@
     <link href="<?=ROOT?>/assets/css/styles.css" rel="stylesheet">
   </head>
   <body>
-  <header class="p-3 text-bg-dark">
+  <header class="p-3">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/home" class="d-flex align-items-center mb-2 mb-lg-0">
             <img class="rounded-circle shadow" src="<?=ROOT?>/assets/images/blog-logo.png" alt="" width="58" height="58">
         </a>
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 ms-lg-5">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Blog</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Single Post</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Pages</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Contact</a></li>
+        <ul class="nav col-12 col-lg-auto text-dark me-lg-auto mb-2 justify-content-center mb-md-0 ms-lg-5">
+          <li><a href="#" class="nav-link px-2 text-dark">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-dark">Blog</a></li>
+          <li><a href="#" class="nav-link px-2 text-dark">Single Post</a></li>
+          <li><a href="#" class="nav-link px-2 text-dark">Pages</a></li>
+          <li><a href="#" class="nav-link px-2 text-dark">Contact</a></li>
         </ul>
 
+        <div class="theme-toggle">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16">
+          <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16">
+          <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+        </svg>
+        <div class="slider"></div>
+        </div>
+
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
+          <input type="search" class="form-control form-control-dark text-dark" placeholder="Search..." aria-label="Search">
         </form>
 
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
+        <div class="text-end text-dark">
+          <button type="button" class="btn btn-primary me-2">Login</button>
           <button type="button" class="btn btn-warning">Sign-up</button>
         </div>
       </div>
     </div>
+    <script src="<?=ROOT?>/assets/js/index.js"></script>
   </header>
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,21 @@
 function display_image_edit(file){
     document.querySelector(".image-preview-edit").src = URL.createObjectURL(file);
 }
+
+const themeToggle = document.querySelector('.theme-toggle');
+const slider = document.querySelector('.slider');
+const sunIcon = document.querySelector('.bi-sun-fill');
+const moonIcon = document.querySelector('.bi-moon-fill');
+
+let isDarkMode = false;
+
+themeToggle.addEventListener('click', () => {
+    isDarkMode = !isDarkMode;
+    const translateValue = isDarkMode ? 'translateX(60px)' : 'translateX(0)';
+    slider.style.transform = translateValue;
+    sunIcon.style.color = isDarkMode ? '#fff' : '#333';
+    moonIcon.style.color = isDarkMode ? '#333' : '#fff';
+
+    // additional logic to toggle website's theme.
+});
+
